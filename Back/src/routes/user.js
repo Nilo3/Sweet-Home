7c6f3090cf7a9a1fc6b08ce7176a8af2e7a306b5
Nilo Echezarreta <nilo.echezarreta@gmail.com>
2023-06-13 18:38:01 -0300

    definidiendo rutas

--- Back/src/routes/user.js
+++ Back/src/routes/user.js
@@ -1,2 +1,8 @@
 const express = require("express");
-const router = express();
+const router = express.Router();
+
+const getAllUsers = require("../controllers/user/getAllUsers");
+
+router.get("/users", getAllUsers);
+
+module.exports = router;
