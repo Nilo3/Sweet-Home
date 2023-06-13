243b3a85ea58f58f2627d14b0efd007c768643a8
ferronsita <fernandarb1396@gmail.com>
2023-06-13 16:56:46 -0600

    Responsive modified

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -3,6 +3,7 @@
 import './App.css'
 import Recommendations from './Components/Recommendations/Recommendations'
 import Footer from './Components/Footer/Footer'
+import MostValued from "./Components/MostValued/MostValued"
 
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
 
   return (
     <>
-      {<Footer/>}
+      {<MostValued/>}
     </>
 
 
--- Front/src/Components/MostValued/MostValued.tsx
+++ Front/src/Components/MostValued/MostValued.tsx
@@ -1,1 +1,13 @@
 import "./MostValued.css"
+
+export default function Footer () {
+    return (
+
+        <>
+        <div className="mostValued">
+        <div className="mostValuedContainer"></div>
+        <p className="valuedTitle">Most Valued Products</p>
+        </div>
+       
+        </>
+    )}
