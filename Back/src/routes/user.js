99e27e7dbca146046d9b9261eeee40172a44a1d0
Nilo Echezarreta <nilo.echezarreta@gmail.com>
2023-06-14 20:18:04 -0300

    hasheado ok

--- Back/src/routes/user.js
+++ Back/src/routes/user.js
@@ -3,6 +3,7 @@ import { Router } from "express";
 import getAllUsers from "../controllers/user/getAllUsers.js";
 import postUser from "../controllers/user/postUser.js";
 import getUserById from "../controllers/user/getUserById.js";
+import deleteUsers from "../controllers/user/deleteUsers.js";
 
 const router = Router();
 
@@ -11,5 +12,7 @@ router.get("/users/:id", getUserById);
 
 //? Ruta .post
 router.post("/users", postUser);
+
+router.delete("users/:id", deleteUsers)
 
 export default router;
