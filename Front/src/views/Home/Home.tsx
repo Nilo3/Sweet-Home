8737a1edb2f78f2c8509213d37f526b2aab0eec6
ferronsita <fernandarb1396@gmail.com>
2023-06-13 19:58:46 -0600

    Fusion between origin/Fernanda and origin/Dev-Front

--- Front/src/views/Home/Home.tsx
+++ Front/src/views/Home/Home.tsx
@@ -1,19 +1,26 @@
 // import { useState } from 'react'
 
 import 'tailwindcss/tailwind.css';
-import Navbar from '../../components/Navbar/Navbar';
+import Navbar from "../../Components/Navbar/Navbar"
+import Footer from '../../Components/Footer/Footer';
+import Recommendations from '../../Components/Recommendations/Recommendations';
+import "./Home.css"
 
 
 function Home() {
   return (
-    <>
+    <body>
    
     <Navbar/>
-    <div className="flex items-center justify-center h-screen bg-orange-200">
+    <div className="flex items-center justify-center h-screen" style={{ backgroundColor: "#EDEDE9", height: "200vh" }}>
       <h1 className="text-4xl text-center">SWEET HOME</h1>
-    
     </div>
-    </>
+    <div className='recommendationContainer'>
+    </div>
+    <footer className='footer'>
+    <Footer/>
+    </footer>
+    </body>
   );
 }
 
