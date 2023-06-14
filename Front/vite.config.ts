c9622d226386eb5821efbbad8495803d4e81660d
INachoMander <ignacio.mander@gmail.com>
2023-06-14 09:42:58 -0300

    aboutUs

--- Front/vite.config.ts
+++ Front/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+import { UserConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+const config: UserConfig = {
+  plugins: [
+    reactRefresh(),
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+      ],
+    },
+  },
+};
+
+export default config;
