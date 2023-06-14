dbee3a3bc01611ccf6122c20f2e390b9dac0627c
hector-granucci <titototoxd456@hotmail.com>
2023-06-14 20:10:26 -0300

    ruta delete y put a probrar

--- Back/src/routes/product.js
+++ Back/src/routes/product.js
@@ -5,6 +5,8 @@ import { Router } from "express";
 import postProduct from "../controllers/product/postProduct.js";
 import getProduct from "../controllers/product/getProducts.js";
 import getProductById from "../controllers/product/getProductById.js";
+import deleteProduct from "../controllers/product/deleteProduct.js";
+import putProduct from "../controllers/product/putProduct.js";
 
 const router = Router();
 
@@ -14,5 +16,11 @@ router.get("/product/:productId", getProductById);
 
 //? Ruta .post
 router.post("/product", postProduct);
+
+//? Ruta .delete
+router.delete("/product/:productId", deleteProduct);
+
+//? Ruta .put
+router.put("/product/:productId", putProduct);
 
 export default router;
