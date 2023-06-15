d1f965ed8171d96edaaa29e38af534bc3cbf2eae
frannpor <frannporciel@gmail.com>
2023-06-15 20:39:10 -0300

    merge with tito

--- Back/src/controllers/order/getOrder.js
+++ Back/src/controllers/order/getOrder.js
@@ -1,20 +1,22 @@
-import Order  from "../../models/schemas/order.js";
-import User from "../../models/schemas/user.js"
+import Order from "../../models/schemas/order.js";
 import Product from "../../models/schemas/product.js";
 
 export default async (req, res) => {
-    try {
-        const orders = await Order.find()
-            .populate({
-                path: "user",
-                model: User,
-            })
-            .populate({
-                path: "product",
-                model: Product,
-            });
-        res.status(200).json(orders);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+  try {
+    const orders = await Order.find({})
+      .populate({
+        path: "user",
+        select: "_id",
+      })
+      .populate({
+        path: "product",
+        select: "_id price",
+        model: Product,
+      })
+      .select("user product totalPrice isPaid padAt");
+      
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
