2e679c79377e0e5d401aff556415f6209af44326
frannpor <frannporciel@gmail.com>
2023-06-15 19:37:40 -0300

    added order get route

--- Back/src/routes/order.js
+++ Back/src/routes/order.js
@@ -2,8 +2,12 @@ import { Router } from "express";
 
 // ? Controllers 
 import postOrder from "../controllers/order/postOrder.js";
+import getOrder from "../controllers/order/getOrder.js";
 
 const router = Router();
+
+//? Rutas .get
+router.get("/order", getOrder)
 
 //? Rutas .post
 router.post("/order", postOrder);
