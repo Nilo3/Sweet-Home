fd39508383ce33e3a007f3975aa96c23ae02f5b9
frannpor <frannporciel@gmail.com>
2023-06-15 18:44:43 -0300

    changed order model

--- Back/src/models/schemas/order.js
+++ Back/src/models/schemas/order.js
@@ -1,52 +1,13 @@
 import mongoose from "mongoose";
+const { Schema } = mongoose;
 
-const orderSchema = mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    require: true,
-    ref: "User"
-  },
-  orderItems: [{
-    name: { type: String, required: true },
-    qty: { type: Number, required: true },
-    image: { type: String, required: true },
-    price: { type: Number, required: true },
-    product: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: "Product",
-    },
-  },
-  ],
-  paymentMethod: {
-    type: String,
-    required: true,
-    default: "Paypal"
-  },
-  paymentResult: {
-    id: { type: String },
-    status: { type: String },
-    update_time: { type: String },
-    email_address: { type: String }
-  },
-  totalPrice: {
-    type: Number,
-    required: true,
-    default: 0.0
-  },
-  isPaid: {
-    type: Boolean,
-    required: true,
-    default: false
-  },
-  padAt: {
-    type: Date
-  },
-},
-  {
-    timestamps: true
-  }
-)
+const orderSchema = new Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, require: true, ref: "User" },
+  product: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Product" },
+  totalPrice: { type: Number, required: true, default: 0.0 },
+  isPaid: { type: Boolean, required: true, default: false },
+  padAt: { type: Date }
+});
 
 const Order = mongoose.model("Order", orderSchema)
 
