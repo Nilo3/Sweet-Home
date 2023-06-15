cc2115ad233eb118dad2cc7ce3218c01e3ed6e80
hector-granucci <titototoxd456@hotmail.com>
2023-06-15 19:39:24 -0300

    ruta delete

--- Back/src/routes/order.js
+++ Back/src/routes/order.js
@@ -2,11 +2,15 @@ import { Router } from "express";
 
 // ? Controllers 
 import postOrder from "../controllers/order/postOrder.js";
+import deleteOrder from "../controllers/order/deleteOrder.js";
 
 const router = Router();
 
 //? Rutas .post
 router.post("/order", postOrder);
+
+//? Rutas .post
+router.delete("/order/:orderId", deleteOrder);
 
 
 export default router;
