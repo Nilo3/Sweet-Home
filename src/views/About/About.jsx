f07e7e545873f7137be23cc3e4c1cfe2264df638
INachoMander <ignacio.mander@gmail.com>
2023-06-16 15:40:09 -0300

    setup-aboutUs

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -1,10 +1,29 @@
+import pocoyo from './pocoyo.jpeg'
+
 const About = ()=>{
 
     return(
-        <div>
-<h1>Estamos en About</h1>
+        <>
+        <div> // Fernanda
+            <img width="500" src={pocoyo} alt="pocoyo" />
         </div>
+        <div> // Francisco
+            <img width="500" src={pocoyo} alt="pocoyo" />
+        </div>
+        <div> // Hector
+            <img width="500" src={pocoyo} alt="pocoyo" />
+        </div>
+        </>
     )
 }
 
 export default About
+
+
+
+
+// Ignacio
+// Nilo
+// Stefano
+// Stella
+// Valentín
