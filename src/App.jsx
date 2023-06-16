a130793b9a3dfc091ee6fe00737162c4e75de60b
ferronsita <fernandarb1396@gmail.com>
2023-06-15 18:06:37 -0600

    Fernanda branch created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-import './App.css'
+
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
 import Products from './views/ShopNow/Products'
