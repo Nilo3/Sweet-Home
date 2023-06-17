a373c550388bcbbb3ea9b0f166668d4f9a1b041f
ferronsita <fernandarb1396@gmail.com>
2023-06-17 14:05:55 -0600

    modifying Recommendations

--- src/components/Recommendations/Recomendations.jsx
+++ src/components/Recommendations/Recomendations.jsx
@@ -7,7 +7,7 @@ export default function Recommendations() {
           <div className="flex w-full">
             <img className="object-cover w-1/2 h-80 md:h-100 rounded-l-lg" src="https://i.pinimg.com/564x/ff/0a/7e/ff0a7e8dba6650b9559659dbc5356956.jpg" alt="" />
             <div className="flex flex-col justify-between p-4 leading-normal w-1/2">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Recomendations </h5>
+              <h5 className="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">Achieve that sweet space</h5>
               <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse necessitatibus eos impedit, dolores error facilis magnam asperiores quis iusto sint eaque blanditiis quas! Deleniti similique quo non tenetur, nisi repellat.
               Pariatur, suscipit quaerat. Doloremque quos mollitia dignissimos consequatur in tempora cumque vitae iusto itaque, expedita reiciendis ratione at architecto aliquid, repellat officiis accusamus non neque, cupiditate reprehenderit odit. Eos, quae?</p>
             </div>
