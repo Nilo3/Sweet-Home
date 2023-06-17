d2d068486dc445b411281074abc4c4b48b581957
ferronsita <fernandarb1396@gmail.com>
2023-06-17 14:31:47 -0600

    Footer links removed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -19,16 +19,16 @@ export default function Footer () {
                       <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
                       <ul className="text-gray-600 dark:text-gray-400 font-medium">
                           <li className="mb-4">
-                              <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Github</a>
+                              <a href="" className="hover:underline ">Github</a>
                           </li >
                           <li className="mb-4">
-                              <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Facebook</a>
+                              <a href="" className="hover:underline">Facebook</a>
                           </li>
                           <li className="mb-4">
-                              <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Twitter</a>
+                              <a href="" className="hover:underline ">Twitter</a>
                           </li>
                           <li>
-                              <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Instagram</a>
+                              <a href="" className="hover:underline">Instagram</a>
                           </li>
                           
                       </ul>
@@ -48,7 +48,7 @@ export default function Footer () {
           </div>
           <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <div className="sm:flex sm:items-center sm:justify-between">
-              <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400 mt-4">© 2023 <a href="https://flowbite.com/" className="hover:underline">Sweet Home</a>.All Rights Reserved. No part of this may be reproduced or transmitted in any form without prior written permission from Sweet Home
+              <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400 mt-4">© 2023 <a href="" className="hover:underline">Sweet Home</a>.All Rights Reserved. No part of this may be reproduced or transmitted in any form without prior written permission from Sweet Home
               </span>
               <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
                   <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
