b869d42114f1c3b417a457a759188a463a52e98c
frannpor <frannporciel@gmail.com>
2023-06-17 04:22:00 -0300

    added route to addProducts

--- Back/src/routes/product.js
+++ Back/src/routes/product.js
@@ -24,3 +24,21 @@ router.delete("/product/:productId", deleteProduct);
 router.put("/product/:productId", putProduct);
 
 export default router;
+
+//? Ruta para postear productos
+
+// import products from "../data/products.json";
+
+// router.post("/product/bulk", async (req, res) => {
+//   if (!Array.isArray(products)) {
+//     return res.status(400).json({ message: "Invalid data format. Expecting an array of products." });
+//   }
+
+//   try {
+//     const createdProducts = await Product.create(products);
+//     return res.json(createdProducts);
+//   } catch (error) {
+//     console.log(error.message);
+//     return res.status(500).json({ message: error.message });
+//   }
+// });
