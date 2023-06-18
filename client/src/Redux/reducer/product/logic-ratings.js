3987eda03640c941db6d772435f90fb4b2fb00cd
ferronsita <fernandarb1396@gmail.com>
2023-06-18 16:35:12 -0600

    most valued completed

--- client/src/Redux/reducer/product/logic-ratings.js
+++ client/src/Redux/reducer/product/logic-ratings.js
@@ -1,40 +1,47 @@
 
 
 export const productAVG = (reviews) => {
-    const ratingTotals = {};
-    const ratingCounts = {};
-    const productAVGArray = [];
-  
-    reviews.forEach((review) => {
-      const productId = review.product;
-      const rating = review.rating;
-  
-      if (!ratingTotals[productId]) {
-        ratingTotals[productId] = 0;
-        ratingCounts[productId] = 0;
-      }
-  
-      ratingTotals[productId] += rating;
-      ratingCounts[productId] += 1;
-    });
-  
-    for (const productId in ratingTotals) {
-      const ratingTotal = ratingTotals[productId];
-      const ratingCount = ratingCounts[productId];
-      const ratingAvg = ratingTotal / ratingCount;
-  
-      productAVGArray.push({
-        product: productId,
-        ratingAvg: ratingAvg
-      });
-    }
-  
-    productAVGArray.sort((a, b) => b.ratingAvg - a.ratingAvg);
-  
-    return productAVGArray;
-  };
+  const ratingTotals = {};
+  const ratingCounts = {};
+  const productAVGArray = [];
 
-//   const reviews = [{"_id":"648d015806921c24d54793b2","rating":5,"createdBy":"648a2c3a59056bfa074393e9","product":"648c07ea49938d2aef4e5292"},{"_id":"648d019f06921c24d54793b9","rating":5,"createdBy":"648a33d7c87102e8b9b681e6","product":"648c07ea49938d2aef4e5292"},{"_id":"648d021906921c24d54793c1","rating":5,"createdBy":"648a4a4c2c0b29a89b108270","product":"648c07ea49938d2aef4e5298"},{"_id":"648d024d06921c24d54793cb","rating":2,"createdBy":"648a4a4c2c0b29a89b108270","product":"648c07ea49938d2aef4e52b7"},{"_id":"648d026f06921c24d54793cf","rating":2,"createdBy":"648a4a4c2c0b29a89b108270","product":"648c07ea49938d2aef4e52b5"},{"_id":"648d02a006921c24d54793d4","rating":2,"createdBy":"648a4b598162867c59eef5c8","product":"648c07ea49938d2aef4e5293"},{"_id":"648d02c606921c24d54793d9","rating":1,"createdBy":"648a4b598162867c59eef5c8","product":"648c07ea49938d2aef4e5297"},{"_id":"648e2079c4f09f6b209dcec2","rating":1,"createdBy":"648a4b598162867c59eef5c8","product":"648c07ea49938d2aef4e52b7"},{"_id":"648e2091c4f09f6b209dcec8","rating":3,"createdBy":"648a4b598162867c59eef5c8","product":"648c07ea49938d2aef4e52b7"},{"_id":"648e2128c4f09f6b209dcecd","rating":2,"createdBy":"648a4b598162867c59eef5c8","product":"648c07ea49938d2aef4e5298"},{"_id":"648e219bc4f09f6b209dced2","rating":3,"createdBy":"648a4b598162867c59eef5c8","product":"648c07ea49938d2aef4e52b5"},{"_id":"648e21b6c4f09f6b209dced7","rating":4,"createdBy":"648a4b598162867c59eef5c8","product":"648c07ea49938d2aef4e5293"},{"_id":"648e21bfc4f09f6b209dcedb","rating":4,"createdBy":"648a4b598162867c59eef5c8","product":"648c07ea49938d2aef4e5297"}]
+  reviews.forEach((review) => {
+    const productId = review.product._id;
+    const rating = review.rating;
+
+    if (!ratingTotals[productId]) {
+      ratingTotals[productId] = 0;
+      ratingCounts[productId] = 0;
+    }
+
+    ratingTotals[productId] += rating;
+    ratingCounts[productId] += 1;
+  });
+
+  for (const productId in ratingTotals) {
+    const ratingTotal = ratingTotals[productId];
+    const ratingCount = ratingCounts[productId];
+    const ratingAvg = ratingTotal / ratingCount;
+
+    const product = reviews.find((review) => review.product._id === productId).product;
+    const { name, price, image } = product;
+
+    productAVGArray.push({
+      product: productId,
+      name: name,
+      price: price,
+      image:image,
+      ratingAvg: ratingAvg,
+    });
+  }
+
+  productAVGArray.sort((a, b) => b.ratingAvg - a.ratingAvg);
+
+  return productAVGArray;
+};
+
+  
+//  const reviews = [{"_id":"648d015806921c24d54793b2","rating":5,"createdBy":{"accessToken":"","_id":"648a2c3a59056bfa074393e9","name":"nilo","email":"admin@example.com","password":"123456","isAdmin":false,"cart":[],"bought":[],"favorites":[],"userReviews":[],"userOrders":[],"__v":0},"product":{"_id":"648c07ea49938d2aef4e5292","name":"Sweet® Cozy Throw Blanket","price":29.99,"image":"https://m.media-amazon.com/images/I/71xZfQ8o7cL._AC_UF894,1000_QL80_.jpg","description":"Wrap yourself in luxury with the Sweet® Cozy Throw Blanket. Made from ultra-soft and plush fabric, this blanket provides exceptional warmth and comfort on chilly evenings. Its elegant design and high-quality craftsmanship make it a stylish addition to any living space. Whether you use it to snuggle up on the couch or drape it over your bed, the Sweet® Cozy Throw Blanket is a must-have for cozy nights and relaxing moments.","stock":50,"category":["648a160cd9329527f0df015f"],"review":[],"createdAt":"2023-06-16T06:57:46.519Z","__v":0},"__v":0},{"_id":"648d019f06921c24d54793b9","rating":5,"createdBy":{"accessToken":"","_id":"648a33d7c87102e8b9b681e6","name":"tito","email":"tito@example.com","password":"123456","isAdmin":false,"cart":[],"bought":[],"favorites":[],"userReviews":[],"userOrders":[],"__v":0},"product":{"_id":"648c07ea49938d2aef4e5292","name":"Sweet® Cozy Throw Blanket","price":29.99,"image":"https://m.media-amazon.com/images/I/71xZfQ8o7cL._AC_UF894,1000_QL80_.jpg","description":"Wrap yourself in luxury with the Sweet® Cozy Throw Blanket. Made from ultra-soft and plush fabric, this blanket provides exceptional warmth and comfort on chilly evenings. Its elegant design and high-quality craftsmanship make it a stylish addition to any living space. Whether you use it to snuggle up on the couch or drape it over your bed, the Sweet® Cozy Throw Blanket is a must-have for cozy nights and relaxing moments.","stock":50,"category":["648a160cd9329527f0df015f"],"review":[],"createdAt":"2023-06-16T06:57:46.519Z","__v":0},"__v":0}]
   
 // console.log(productAVG(reviews))
   
---
+++ client/src/components/MostValue/mostValueCards.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const MostValueCards = ({ id, name, image, price, rating }) => {
+  const renderRatingStars = () => {
+    const maxRating = 5;
+    const fullStars = Math.floor(rating);
+    const hasHalfStar = rating % 1 !== 0;
+    const emptyStars = maxRating - fullStars - (hasHalfStar ? 1 : 0);
+
+    const stars = [];
+    for (let i = 0; i < fullStars; i++) {
+      stars.push(<span key={i} className="star-icon">&#9733;</span>);
+    }
+
+    if (hasHalfStar) {
+      stars.push(<span key="half" className="star-icon">&#9733;</span>);
+    }
+
+    for (let i = 0; i < emptyStars; i++) {
+      stars.push(<span key={`empty-${i}`} className="star-icon">&#9734;</span>);
+    }
+
+    return stars;
+  };
+
+  return (
+    <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 h-full flex flex-col justify-between">
+      <div>
+        <Link to={`/products/${id}`}>
+          <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+            {name}
+          </h5>
+        </Link>
+        <img src={image} alt={name} className="h-48 object-cover mx-auto" />
+        <div className="flex items-center">
+          <div className="rating-stars">{renderRatingStars()}</div>
+          <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-auto ">
+            {rating}
+          </span>
+        </div>
+      </div>
+      <div className="flex items-center justify-between">
+        <span className="text-3xl font-bold text-gray-900 dark:text-white">
+          ${price}
+        </span>
+        <a
+          href="#"
+          className="text-white bg-black hover:bg-neutral-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+        >
+          Add to cart
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default MostValueCards;
