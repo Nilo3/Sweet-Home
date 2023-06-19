0f263a8c2a36f469996f2aed0ef2d3b34f7728b9
Valen <valenzalazar3@gmail.com>
2023-06-19 15:52:08 -0300

    actualizacion

--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -22,13 +22,13 @@ const Pagination = ({productPerPage, currentPage, setCurrentPage, totalProducts}
 <nav aria-label="Page navigation example ">
   <ul className="inline-flex -space-x-px">
     <li>
-      <a className="px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white" onClick={onPreviusPage}>Previous</a>
+      <a className="px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"  onClick={onPreviusPage}>Previous</a>
     </li>
     {
       pageNumbers.map((noPage) => (
         
         <li key= {noPage}>
-        <a className= "px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"  onClick={() => setCurrentPage(noPage)}>{noPage}</a>
+        <a className= {`px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`} onClick={() => setCurrentPage(noPage)}>{noPage}</a>
       </li>
     
       ))
