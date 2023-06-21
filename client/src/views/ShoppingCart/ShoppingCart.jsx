d569c5853651737ec4304bdaabda7f850363cf55
ferronsita <fernandarb1396@gmail.com>
2023-06-21 12:45:33 -0600

    shipping options added to shoppingCart

--- client/src/views/ShoppingCart/ShoppingCart.jsx
+++ client/src/views/ShoppingCart/ShoppingCart.jsx
@@ -2,6 +2,8 @@
 import { useDispatch, useSelector } from "react-redux";
 import { removefromCart } from "../../Redux/actions/product/productActions";
 import {getTotalPrice, calculateTotal} from "./totalprice"
+import fedexLogo from "../../assets/image/Fedex-logo.jpeg"
+import dhlLogo from "../../assets/image/DHL-logo.png"
 
 const Shopping = () => {
     const allShoppingCart = useSelector((state) => state.shoppingCart);
@@ -49,31 +51,30 @@ const Shopping = () => {
           <span className="font-semibold text-gray-500">Payment</span>
         </li>
             </ul>
-        </div>
-        </div>
-        </div>
-        <div className="grid sm:px-10 lg:grid-cols-2 lg:px-20 xl:px-32">
-        <div className="px-4 pt-8">
-        <p className="text-xl font-medium">Order Summary</p>
-        <p className="text-gray-400">Check your items. And select a suitable shipping method.</p>
-        <div className="mt-8 space-y-3 rounded-lg border bg-white px-2 py-4 sm:px-6">
-        {allShoppingCart?.length === 0 ? (
-        <p className="text-gray-400 flex items-center justify-center">Your shopping Cart is empty.</p>
-      ) : (
-        allShoppingCart.map((prod) => (
-            <div key={prod.id} className="flex flex-col rounded-lg bg-white sm:flex-row">
-             <img className="m-2 h-24 w-28 rounded-md border object-cover object-center" src={prod.image} alt="" />
-            <div className="flex w-full flex-col px-4 py-4">
-           <div className="flex items-center justify-between">
-           <span className="font-semibold">{prod.name}</span>
-           <button className="ml-auto" onClick={() => handleDeleteFromCart(prod.id)}>X</button>
            </div>
-           <p className="text-lg font-bold">${prod.price}</p>
+           </div>
             </div>
-            </div>
+             <div className="grid sm:px-10 lg:grid-cols-2 lg:px-20 xl:px-32">
+            <div className="px-4 pt-8">
+            <p className="text-xl font-medium">Order Summary</p>
+            <p className="text-gray-400">Check your items. And select a suitable shipping method.</p>
+             <div className="mt-8 space-y-3 rounded-lg border bg-white px-2 py-4 sm:px-6">
+              {allShoppingCart?.length === 0 ? (
+              <p className="text-gray-400 flex items-center justify-center">Your shopping Cart is empty.</p>
+              ) : (
+              allShoppingCart.map((prod) => (
+              <div key={prod.id} className="flex flex-col rounded-lg bg-white sm:flex-row">
+             <img className="m-2 h-24 w-28 rounded-md border object-cover object-center" src={prod.image} alt="" />
+              <div className="flex w-full flex-col px-4 py-4">
+              <div className="flex items-center justify-between">
+               <span className="font-semibold">{prod.name}</span>
+               <button className="ml-auto" onClick={() => handleDeleteFromCart(prod.id)}>X</button>
+               </div>
+               <p className="text-lg font-bold">${prod.price}</p>
+               </div>
+               </div>
       )))}
               </div>
-
             </div>
         <div className="mt-10 bg-gray-50 px-4 pt-8 lg:mt-0 mr:auto">
     <p className="text-xl font-medium">Payment Details</p>
@@ -122,16 +123,43 @@ const Shopping = () => {
         </div>
         <div className="flex items-center justify-between">
           <p className="text-sm font-medium text-gray-900">Shipping</p>
-          <p className="font-semibold text-gray-900">$8.00</p>
+          <p className="font-semibold text-gray-900">${shippingRate}</p>
         </div>
       </div>
       <div className="mt-6 flex items-center justify-between">
         <p className="text-sm font-medium text-gray-900">Total</p>
         <p className="text-2xl font-semibold text-gray-900">${total}</p>
       </div>
-      
-    <button className="mt-4 mb-8 w-full rounded-md bg-gray-900 px-6 py-3 font-medium text-white">Place Order</button>
+   <p className="mt-8 text-lg font-medium">Shipping Methods</p>
+  
+              <form className="mt-5 grid gap-6">
+              <div className="relative">
+               <input className="peer hidden" id="radio_1" type="radio" name="radio" checked />
+                <span className="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
+                <label className="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4" for="radio_1">
+                <img className="w-14 object-contain" src={fedexLogo} alt="" />
+                <div className="ml-5">
+              <span className="mt-2 font-semibold">Fedex Delivery</span>
+              <p className="text-slate-500 text-sm leading-6">Delivery: 2-4 Days</p>
+          </div>
+                </label>  
+              </div>
+              <div className="relative">
+               <input className="peer hidden" id="radio_1" type="radio" name="radio" checked />
+                <span className="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
+                <label className="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4" for="radio_1">
+                <img className="w-14 object-contain" src={dhlLogo} alt="" />
+                <div className="ml-5">
+              <span className="mt-2 font-semibold">DHL Delivery</span>
+              <p className="text-slate-500 text-sm leading-6">Delivery: 2-4 Days</p>
+          </div>
+                </label>  
+              </div>
+                </form>
+
+                <button className="mt-4 mb-8 w-full rounded-md bg-gray-900 px-6 py-3 font-medium text-white">Place Order</button>
     </div>
+    
     </div>
         </>
 
