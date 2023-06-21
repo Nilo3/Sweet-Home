f8176343414063223ed3da11f249b5b11b91ca7f
ferronsita <fernandarb1396@gmail.com>
2023-06-21 08:27:18 -0600

    git detail modified

--- client/src/views/Detail/Detail.jsx
+++ client/src/views/Detail/Detail.jsx
@@ -11,34 +11,7 @@ const Detail = () => {
     dispatch(getProductDetail(id));
   }, [dispatch, id]);
 
-  <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 h-full flex flex-col justify-between mx-auto my-auto">
-  <div>
-    <NavLink to={`/products/${product._id}`}>
-      <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
-        {product.name}
-      </h5>
-    </NavLink>
-    <img
-      src={product.image}
-      alt={product.name}
-      className="h-48 object-cover mx-auto"
-    />
-    <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
-      {product.description}
-    </p>
-  </div>
-  <div className="flex items-center justify-between">
-    <span className="text-3xl font-bold text-gray-900 dark:text-white">
-      ${product.price}
-    </span>
-    <a
-      href="#"
-      className="text-white bg-black hover:bg-neutral-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-    >
-      Add to cart
-    </a>
-  </div>
-</div>
+  
   return (
    
     <section className="py-12 sm:py-16"> 
