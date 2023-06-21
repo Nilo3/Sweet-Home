81e150bff5e9e987f314c45fd1761eafd22822db
ferronsita <fernandarb1396@gmail.com>
2023-06-20 22:47:47 -0600

    Shopping Cart modified

--- client/src/views/ShoppingCart/ShoppingCart.jsx
+++ client/src/views/ShoppingCart/ShoppingCart.jsx
@@ -11,7 +11,7 @@ const Shopping = () => {
       <>
        
         <div className="flex flex-col items-center border-b bg-white py-4 sm:flex-row sm:px-10 lg:px-20 xl:px-32">
-        <a href="#" className="text-2xl font-bold text-gray-800">Sweet Home</a>
+        <a href="#" className="text-2xl font-bold text-gray-800">Checkout</a>
         <div className="mt-4 py-2 text-xs sm:mt-0 sm:ml-auto sm:text-base">
         <div className="relative">
         <ul className="relative flex w-full items-center justify-between space-x-2 sm:space-x-4">
