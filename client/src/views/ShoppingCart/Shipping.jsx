8e0422b0fbaf31e584e16747c4ba2b2ee38081ca
ferronsita <fernandarb1396@gmail.com>
2023-06-22 16:22:20 -0600

    Shipping modified

--- client/src/views/ShoppingCart/Shipping.jsx
+++ client/src/views/ShoppingCart/Shipping.jsx
@@ -59,26 +59,29 @@ const Shipping = ({ id, name, image, price }) => {
         <div className="relative">
         <ul className="relative flex w-full items-center justify-between space-x-2 sm:space-x-4">
         <li className="flex items-center space-x-3 text-left sm:space-x-4">
-        <a className="flex h-6 w-6 items-center justify-center rounded-full bg-blue-200 text-xs font-semibold  text-blue-700" href="#">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
-                </svg></a>
-             <span className="font-semibold text-gray-900">Shop</span>
-            </li>
+          <a className="flex h-6 w-6 items-center justify-center rounded-full bg-gray-400 text-xs font-semibold text-white" href="#">1</a>
+          <span className="font-semibold text-gray-500">Shop</span>
+        </li>
+        
             <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
           <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
         </svg>
         <li className="flex items-center space-x-3 text-left sm:space-x-4">
-          <a className="flex h-6 w-6 items-center justify-center rounded-full bg-gray-600 text-xs font-semibold text-white ring ring-gray-600 ring-offset-2" href="#">2</a>
-          <span className="font-semibold text-gray-900">Shipping</span>
-        </li>
+        <a className="flex h-6 w-6 items-center justify-center rounded-full bg-blue-200 text-xs font-semibold  text-blue-700" href="#">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
+                </svg></a>
+             <span className="font-semibold text-gray-900">Shipping</span>
+            </li>
+        
         <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
           <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
         </svg>
         <li className="flex items-center space-x-3 text-left sm:space-x-4">
-          <a className="flex h-6 w-6 items-center justify-center rounded-full bg-gray-400 text-xs font-semibold text-white" href="#">3</a>
-          <span className="font-semibold text-gray-500">Payment</span>
+          <a className="flex h-6 w-6 items-center justify-center rounded-full bg-gray-600 text-xs font-semibold text-white ring ring-gray-600 ring-offset-2" href="#">3</a>
+          <span className="font-semibold text-gray-900">Payment</span>
         </li>
+
             </ul>
            </div>
            </div>
