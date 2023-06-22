5c8f14867e3360a09dfcec8b3bfce3829daf26a7
INachoMander <ignacio.mander@gmail.com>
2023-06-22 15:17:05 -0300

    styles-and-bugs-fixed

--- client/src/components/Navbar/Buttons/RecommendationsNav.jsx
+++ client/src/components/Navbar/Buttons/RecommendationsNav.jsx
@@ -11,9 +11,9 @@ const RecomendationsNav = () => {
       <div className="flex flex-row items-center gap-3 select-none">
         <div className="p-4 md:py-1 md:px-2 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
           <BsFillChatSquareHeartFill className="text-2xl" />
-          <button className="hidden md:block" onClick={navigateToProducts}>
+          <a className="hidden md:block" onClick={navigateToProducts}>
             Recommendations
-          </button>
+          </a>
         </div>
       </div>
     </div>
