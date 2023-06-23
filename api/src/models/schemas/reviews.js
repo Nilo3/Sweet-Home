33b77de3cc3e466eca7ddaeb401b1b8e0ac9dc69
frannpor <frannporciel@gmail.com>
2023-06-23 00:20:08 -0300

    added reviews-adder bulk route

--- api/src/models/schemas/reviews.js
+++ api/src/models/schemas/reviews.js
@@ -3,6 +3,7 @@ const { Schema } = mongoose
 
 const review = new Schema({
     rating: { type: Number, min: 1, max: 5, require: true },
+    reviewText: { type: String},
     createdBy: { type: Schema.Types.ObjectId, ref: "User" },
     product: { type: Schema.Types.ObjectId, ref: "Product" },
 });
