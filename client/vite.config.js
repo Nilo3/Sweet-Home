684000152b1dfee7e661acb04b8bb7ffd8dda39e
StefBravi <76262778+StefBravi@users.noreply.github.com>
2023-06-24 20:07:34 -0300

    Modificacion de archivos para el deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 export default defineConfig({
   plugins: [react()],
 })
