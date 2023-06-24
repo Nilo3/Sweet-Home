14eccd3c5e8d9cee9f77734e35010974640abb2c
hector-granucci <titototoxd456@hotmail.com>
2023-06-24 06:18:58 -0300

    arreglo

--- api/src/controllers/order/postOrder.js
+++ api/src/controllers/order/postOrder.js
@@ -5,7 +5,7 @@ import Order from "../../models/schemas/order.js";
 
 export default async (req, res) => {
   try {
-    const { user, product } = req.body;
+    const { user, product, quantity } = req.body;
 
     const foundUser = await User.findOne({ _id: user });
     const foundProducts = await Product.find({ _id: { $in: product } });
@@ -17,16 +17,18 @@ export default async (req, res) => {
     //   return res.status(400).json({ message: "One or more products not found" });
     // }
 
+
     let totalPrice = 0;
-    for (const foundProduct of foundProducts) {
-      totalPrice += foundProduct.price;
-    }
+for (const foundProduct of foundProducts) {
+  const productQuantity = quantity || foundProducts.length; // Considerar la cantidad proporcionada en el cuerpo de la solicitud o el número de productos encontrados
+  totalPrice += foundProduct.price * productQuantity;
+}
 
     const newOrder = new Order({
       user: foundUser._id,
       product: foundProducts.map((foundProduct) => foundProduct._id),
-      quantity: foundProducts.length,
-      totalPrice,
+      quantity: quantity || foundProducts.length,
+      totalPrice: totalPrice/foundProducts.length || totalPrice/quantity,
       padAt: new Date()
     });
    
@@ -37,11 +39,15 @@ export default async (req, res) => {
       access_token: "TEST-373757202745327-062218-fa0a61220d206b6b0540c92d588ceb20-1405850896"
     });
 
+
+    const title = foundProducts.map((foundProduct) => foundProduct.name).join(", ");
+   
+
     const preference = {
       items: [
         {
-          title: "Muebles",
-          unit_price: totalPrice/foundProducts.length,
+          title: title,
+          unit_price: totalPrice/(foundProducts.length*2),
           currency_id: "ARS",
           quantity: foundProducts.length,
         }
@@ -53,7 +59,9 @@ export default async (req, res) => {
       }
     };
 
-  
+    if (foundProducts.length < 2) {
+      preference.items[0].unit_price = totalPrice;
+    }
 
     const result = await mercadopago.preferences.create(preference);
     console.log(result);
