ba0247ed1007ec7716937e75630cd96633ed73b1
MariaFerRonB <122755121+MariaFerRonB@users.noreply.github.com>
2023-06-26 17:03:36 -0600

    review number added to the reviews

--- client/src/views/Detail/Detail.jsx
+++ client/src/views/Detail/Detail.jsx
@@ -355,9 +355,15 @@ const Detail = () => {
                       key={review._id}
                     >
                       <figure className="max-w-screen-md">
-                        <div className="mb-3 text-yellow-300 text-m flex mr-3">
-                          {renderRatingStars(review.rating)}
-                        </div>
+                      <div className="mb-3 flex items-center">
+  <div className="text-yellow-300 text-m flex mr-3">
+    {renderRatingStars(review.rating)}
+  </div>
+  <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">
+    {review.rating}
+  </span>
+</div>
+                        
                         <blockquote>
                           <p className="text-l font-normal text-gray-900 dark:text-white">
                             {review.reviewText}
