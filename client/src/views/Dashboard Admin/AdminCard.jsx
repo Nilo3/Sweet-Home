4dbaf6ad31dc6f354eb8cbee2eca100240f8b698
Valen <valenzalazar3@gmail.com>
2023-06-28 16:27:09 -0300

    stilosAD

--- client/src/views/Dashboard Admin/AdminCard.jsx
+++ client/src/views/Dashboard Admin/AdminCard.jsx
@@ -19,14 +19,14 @@ const AdminCard = ({ id, name, price,description, image, category }) => {
   }
 
   return (
-    <div>
-      <tbody>
-        <tr className="border-b dark:border-gray-700">
+    <div className="flex justify-center w-full ">
+      <tbody className="flex justify-center w-full ">
+        <tr className="border-b dark:border-gray-700 flex justify-between w-full">
           <th
             scope="row"
             className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white"
           >
-            {name}&#34;
+            {name}
           </th>
 
           <td className="px-4 py-3">{category}</td>
@@ -35,12 +35,12 @@ const AdminCard = ({ id, name, price,description, image, category }) => {
           <td className="px-4 py-3 flex items-center justify-end">
       
        <Link to ={`/editProduct/${id}`}>
-       <button>
+       <button className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
             Edit
            </button>
        </Link>
            
-            <button value={id} onClick={(event) => handleDelete(event)}>Delete</button>
+            <button className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900" value={id} onClick={(event) => handleDelete(event)}>Delete</button>
           </td>
         </tr>
       </tbody>
