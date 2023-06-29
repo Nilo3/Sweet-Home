3313c143b143082123ea444dbf15dcd874024f05
INachoMander <ignacio.mander@gmail.com>
2023-06-29 11:08:03 -0300

    Recomendaciones y Top responsive pero ocupa toda la pantalla

--- client/src/components/TopWeek/TopWeek.jsx
+++ client/src/components/TopWeek/TopWeek.jsx
@@ -3,12 +3,8 @@ export default function TopWeek() {
     <>
       <br />
       <div className="w-screen">
-        <a
-          href="#"
-          className="flex items-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mx-auto"
-          style={{ width: "75%" }}
-        >
-          <div className="flex flex-col justify-between p-4 leading-normal w-1/2">
+        <div className="flex flex-col md:flex-row items-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mx-auto">
+          <div className="flex flex-col justify-between p-4 leading-normal w-full md:w-1/2">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               Top of the week
             </h5>
@@ -16,15 +12,15 @@ export default function TopWeek() {
               Discover the latest trends and styles for your home. From
               furniture and decor to appliances and accessories, find everything
               you need to transform your space into a haven of comfort and
-              beauty. Dont miss out on our top picks of the week!
+              beauty. Don't miss out on our top picks of the week!
             </p>
           </div>
           <img
-            className="object-cover w-1/2 h-80 md:h-100 rounded-r-lg"
+            className="object-cover w-full md:w-1/2 h-80 md:h-auto md:max-h-100 rounded-lg md:rounded-r-lg"
             src="https://cc.tvbs.com.tw/img/program/upload/2022/02/17/20220217151643-9c8ac8de.jpg"
             alt="example"
           />
-        </a>
+        </div>
       </div>
     </>
   );
