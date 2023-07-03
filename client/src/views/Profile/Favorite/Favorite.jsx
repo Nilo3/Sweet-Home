75b15b978808e18e68991aa339ffa56e33eb03e4
StefBravi <76262778+StefBravi@users.noreply.github.com>
2023-07-03 11:02:41 -0300

    modificaciones-merge-fran-test/fran-test-

--- client/src/views/Profile/Favorite/Favorite.jsx
+++ client/src/views/Profile/Favorite/Favorite.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import {addtoFavorites, removefromFavorites, getFavorites, postFavorites} from "../../../Redux/actions/actions.js";
