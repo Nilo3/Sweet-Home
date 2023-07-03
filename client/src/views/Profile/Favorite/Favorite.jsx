3b48f756666f97b0b0a65ef89b169fc4f59117c8
frannpor <frannporciel@gmail.com>
2023-07-03 11:32:59 -0300

    changes in favorites

--- client/src/views/Profile/Favorite/Favorite.jsx
+++ client/src/views/Profile/Favorite/Favorite.jsx
@@ -1,13 +1,14 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import {addtoFavorites, removefromFavorites, getFavorites, postFavorites} from "../../../Redux/actions/actions.js";
+import { addtoFavorites, removefromFavorites, getFavorites, postFavorites } from "../../../Redux/actions/actions.js";
 import Pagination from "../../../components/Pagination/Pagination.jsx";
+import { useAuth } from "../../../context/authContex.jsx";
 
 function Favorite() {
   const dispatch = useDispatch();
   const favoriteProducts = useSelector((state) => state.favorites);
-
+  const { user } = useAuth();
   const [currentPage, setCurrentPage] = useState(1);
   const favoriteProductsPerPage = 8;
 
@@ -30,16 +31,16 @@ function Favorite() {
     dispatch(addtoFavorites(product));
     dispatch(postFavorites(payload))
       .then(() => {
-        console.log("Favorito guardado en el servidor");
+        console.log("Favorite saved on the server");
       })
       .catch((error) => {
-        console.log("Error al guardar el favorito en el servidor:", error);
+        console.log("Error saving the favorite on the server:", error);
       });
   };
 
   const indexOfLastProduct = currentPage * favoriteProductsPerPage;
   const indexOfFirstProduct = indexOfLastProduct - favoriteProductsPerPage;
-  const favoriteProductsToDisplay = favoriteProducts.slice(
+  const favoriteProductsToDisplay = favoriteProducts?.slice(
     indexOfFirstProduct,
     indexOfLastProduct
   );
@@ -49,59 +50,56 @@ function Favorite() {
   };
 
   return (
-    <div>
-      <section className="flex flex-col items-center bg-white">
-        <h1 className="mt-10 text-4xl font-bold text-gray-800">My Favorites</h1>
+    <div className="pt-8 flex flex-col items-center justify-center bg-white w-full py-4 sm:flex-row sm:px-10 lg:px-20 xl:px-32">
+      <div className="grid w-3/4 select-none">
+        <p className="text-3xl font-medium mb-4">My favorites</p>
         <div className="mt-10 grid max-w-md grid-cols-1 gap-6 px-2 sm:max-w-lg sm:px-20 md:max-w-screen-xl md:grid-cols-2 md:px-10 lg:grid-cols-3 lg:gap-8">
-          {favoriteProductsToDisplay.length > 0 ? (
-            favoriteProductsToDisplay.map((product) => (
-              <article
-                key={product._id}
-                className="mb-4 overflow-hidden rounded-xl border text-gray-700 shadow-md duration-500 ease-in-out hover:shadow-xl"
-              >
-                <Link to={`/products/${product._id}`}>
+          {favoriteProductsToDisplay?.length > 0 ? (
+            favoriteProductsToDisplay?.map((product) => (
+              <Link key={product?._id} to={`/products/${product?._id}`}>
+                <article className="mb-4 overflow-hidden rounded-xl border text-gray-700 shadow-md duration-500 ease-in-out hover:shadow-xl">
                   <div>
                     <img
-                      src={product.image}
-                      alt={product.name}
+                      src={product?.image}
+                      alt={product?.name}
                       className="w-full h-[300px] object-cover"
                     />
                   </div>
+
                   <div className="p-4 flex flex-col justify-between">
                     <div>
-                      <h3 className="text-lg font-semibold text-center">
-                        {product.name}
+                      <h3 className="text-lg font-semibold text-center overflow-hidden overflow-ellipsis whitespace-nowrap">
+                        {product?.name}
                       </h3>
                       <p className="text-sm text-gray-500">
-                        {product.category}
+                        {product?.category}
                       </p>
                     </div>
+
+                    <button
+                      onClick={() => handleRemoveFavorite(product?._id)}
+                      className="mt-2 px-4 py-2 bg-black text-white rounded-md self-center"
+                    >
+                      Remove from Favorites
+                    </button>
                   </div>
-                </Link>
-                <div className="p-4 flex justify-center">
-                  <button
-                    onClick={() => handleRemoveFavorite(product._id)}
-                    className="mt-2 px-4 py-2 bg-gray-500 text-white rounded-md"
-                  >
-                    Remove from Favorites
-                  </button>
-                </div>
-              </article>
+                </article>
+              </Link>
             ))
           ) : (
             <p>No favorite products found.</p>
           )}
         </div>
 
-        {favoriteProductsToDisplay.length > 0 && (
+        {favoriteProductsToDisplay?.length > 0 && (
           <Pagination
             productPerPage={favoriteProductsPerPage}
             currentPage={currentPage}
             setCurrentPage={handlePageChange}
-            totalProducts={favoriteProducts.length}
+            totalProducts={favoriteProducts?.length}
           />
         )}
-      </section>
+      </div>
     </div>
   );
 }
