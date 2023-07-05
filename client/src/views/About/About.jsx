ae57f5a9808b6a1b2330866e3c09becce901367c
INachoMander <ignacio.mander@gmail.com>
2023-07-05 09:16:40 -0300

    About con Tecnologías

--- client/src/views/About/About.jsx
+++ client/src/views/About/About.jsx
@@ -1,19 +1,30 @@
 import style from "./About.module.css";
 import { FaGithub, FaEnvelope, FaLinkedin } from "react-icons/fa";
 import { teamMembers } from "./profiles/profiles";
-import reactLog from "../../assets/image/reactlogo.png"
-import trelloLog from "../../assets/image/trellogo.svg"
-import discordLog from "../../assets/image/discordlogo.png"
-import firebaseLog from "../../assets/image/firebaselogo.svg"
-import githubLog from "../../assets/image/githublogo.svg"
-import pagoLog from "../../assets/image/mercadopagologo.webp"
-import mongoLog from "../../assets/image/mongoDBlogo.svg"
-import tailLog from "../../assets/image/tailwindcsslogo.svg"
+import { tecnologies } from "./tecnologies/tecnologies";
 
 const About = () => {
   return (
-    <div className={style.aboutContainer}>
-      <h1 className={style.title}>ABOUT THE DEVELOPERS</h1>
+    <div className="text-center mx-auto max-w-6xl px-2 select-none">
+      <div className={style.intro}>
+        <h1 className="text-4xl font-bold mb-4 mt-4">About us</h1>
+        <p className="mb-8">
+          Welcome to our Henry's Full Stack development team. We are a group of
+          8 students passionate about the world of web development. We decided
+          to come together and form a cohesive team with the goal of creating
+          this exceptional e-commerce platform. We stand out for our
+          collaborative approach and strong teamwork. Each member brings their
+          unique skills and knowledge, allowing us to tackle challenging
+          projects with confidence and creativity. Our main objective is to
+          exceed our clients' expectations and deliver innovative and
+          high-quality solutions. We are driven by a constant effort to improve
+          and learn new technologies. We stay updated with the latest trends in
+          web development to provide modern and efficient solutions. We are
+          committed to continuing our professional growth and taking our
+          projects to the next level. We thrive on challenges and we are ready
+          to bring your ideas to life!
+        </p>
+      </div>
 
       <div className={style.container}>
         <div className={style.row}>
@@ -35,7 +46,7 @@ const About = () => {
                     rel="noopener noreferrer"
                     className={style.contactIcon}
                   >
-                    <FaGithub />
+                    <FaGithub size={30} />
                   </a>
                   <a
                     href={member.gmail}
@@ -43,7 +54,7 @@ const About = () => {
                     rel="noopener noreferrer"
                     className={style.contactIcon}
                   >
-                    <FaEnvelope />
+                    <FaEnvelope size={30} />
                   </a>
                   <a
                     href={member.linkedin}
@@ -51,7 +62,7 @@ const About = () => {
                     rel="noopener noreferrer"
                     className={style.contactIcon}
                   >
-                    <FaLinkedin />
+                    <FaLinkedin size={30} />
                   </a>
                 </div>
               </div>
@@ -77,7 +88,7 @@ const About = () => {
                     rel="noopener noreferrer"
                     className={style.contactIcon}
                   >
-                    <FaGithub />
+                    <FaGithub size={30} />
                   </a>
                   <a
                     href={member.gmail}
@@ -85,7 +96,7 @@ const About = () => {
                     rel="noopener noreferrer"
                     className={style.contactIcon}
                   >
-                    <FaEnvelope />
+                    <FaEnvelope size={30} />
                   </a>
                   <a
                     href={member.linkedin}
@@ -93,7 +104,7 @@ const About = () => {
                     rel="noopener noreferrer"
                     className={style.contactIcon}
                   >
-                    <FaLinkedin />
+                    <FaLinkedin size={30} />
                   </a>
                 </div>
               </div>
@@ -101,163 +112,27 @@ const About = () => {
           ))}
         </div>
       </div>
-      <h2 className={style.title}>TECHNOLOGIES APPLIED TO THE PROJECT</h2>
-      <div className={style.row}>
-        <div className={style.card}>
-          <div className={style.imageContainer}>
-            <div
-              className={style.image}
-              style={{ backgroundImage: `url(${reactLog})` }}
-            />
+
+      <h1 className="text-4xl font-bold mt-16 mb-4">Tecnologías 🦾</h1>
+      <div className="grid grid-cols-1 gap-10 md:grid-cols-2">
+        {tecnologies.map((technology, index) => (
+          <div key={index} className="bg-white p-8 shadow rounded">
+            <div className="mb-4">
+              <a
+                href={technology.link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={technology.image}
+                  alt={technology.title}
+                  className="w-24 h-24 object-contain mx-auto"
+                />
+              </a>
+            </div>
+            <p className="text-gray-900 text-sm">{technology.description}</p>
           </div>
-          <div className={style.info}>
-            <h4 className={style.name}>React</h4>
-            <p className={style.description}>
-              React is a JavaScript library for building user interfaces.
-            </p>
-          </div>
-        </div>
-        <div className={style.card}>
-          <div className={style.imageContainer}>
-            <div
-              className={style.image}
-              style={{ backgroundImage: `url(${trelloLog})` }}
-            />
-          </div>
-          <div className={style.info}>
-            <h4 className={style.name}>Trello</h4>
-            <p className={style.description}>
-              Trello is a web-based project management and collaboration tool.
-            </p>
-          </div>
-        </div>
-        {/* Add more cards for other technologies */}
-        <div className={style.card}>
-          <div className={style.imageContainer}>
-            <div
-              className={style.image}
-              style={{ backgroundImage: `url(${discordLog})` }}
-            />
-          </div>
-          <div className={style.info}>
-            <h4 className={style.name}>Discord</h4>
-            <p className={style.description}>
-              React is a JavaScript library for building user interfaces.
-            </p>
-          </div>
-        </div>
-        <div className={style.card}>
-          <div className={style.imageContainer}>
-            <div
-              className={style.image}
-              style={{ backgroundImage: `url(${firebaseLog})` }}
-            />
-          </div>
-          <div className={style.info}>
-            <h4 className={style.name}>Tailwind</h4>
-            <p className={style.description}>
-              React is a JavaScript library for building user interfaces.
-            </p>
-          </div>
-        </div>
-        <div className={style.card}>
-          <div className={style.imageContainer}>
-            <div
-              className={style.image}
-              style={{ backgroundImage: `url(${githubLog})` }}
-            />
-          </div>
-          <div className={style.info}>
-            <h4 className={style.name}>MongoDB</h4>
-            <p className={style.description}>
-              React is a JavaScript library for building user interfaces.
-            </p>
-          </div>
-        </div>
-        <div className={style.card}>
-          <div className={style.imageContainer}>
-            <div
-              className={style.image}
-              style={{ backgroundImage: `url(${pagoLog})` }}
-            />
-          </div>
-          <div className={style.info}>
-            <h4 className={style.name}>Mercado Pago</h4>
-            <p className={style.description}>
-              React is a JavaScript library for building user interfaces.
-            </p>
-          </div>
-        </div>
-        <div className={style.card}>
-          <div className={style.imageContainer}>
-            <div
-              className={style.image}
-              style={{ backgroundImage: `url(${mongoLog})` }}
-            />
-          </div>
-          <div className={style.info}>
-            <h4 className={style.name}>Firebase</h4>
-            <p className={style.description}>
-              React is a JavaScript library for building user interfaces.
-            </p>
-          </div>
-        </div>
-        <div className={style.card}>
-          <div className={style.imageContainer}>
-            <div
-              className={style.image}
-              style={{ backgroundImage: `url(${tailLog})` }}
-            />
-          </div>
-          <div className={style.info}>
-            <h4 className={style.name}>Mongoose</h4>
-            <p className={style.description}>
-              React is a JavaScript library for building user interfaces.
-            </p>
-          </div>
-        </div>
-        <div className={style.card}>
-          <div className={style.imageContainer}>
-            <div
-              className={style.image}
-              style={{ backgroundImage: `url(${reactLog})` }}
-            />
-          </div>
-          <div className={style.info}>
-            <h4 className={style.name}>Git + GitHub</h4>
-            <p className={style.description}>
-              React is a JavaScript library for building user interfaces.
-            </p>
-          </div>
-        </div>
-        <div className={style.card}>
-          <div className={style.imageContainer}>
-            <div
-              className={style.image}
-              style={{ backgroundImage: `url(${reactLog})` }}
-            />
-          </div>
-          <div className={style.info}>
-            <h4 className={style.name}>Visual Studio Code</h4>
-            <p className={style.description}>
-              React is a JavaScript library for building user interfaces.
-            </p>
-          </div>
-        </div>
-        <div className={style.card}>
-          <div className={style.imageContainer}>
-            <div
-              className={style.image}
-              style={{ backgroundImage: `url(${reactLog})` }}
-            />
-          </div>
-          <div className={style.info}>
-            <h4 className={style.name}>Vite</h4>
-            <p className={style.description}>
-              React is a JavaScript library for building user interfaces.
-            </p>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
